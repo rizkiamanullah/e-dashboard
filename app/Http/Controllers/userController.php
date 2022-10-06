bdb0b214ebc31ff2680bc606c281bd0fa85203f1
Rizki Amanullah Hakim <rizkiamanullah723@gmail.com>
2022-10-06 17:25:35 +0700

    Initial commit

---
+++ app/Http/Controllers/userController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class userController extends Controller
+{
+    //
+
+    public function __construct()
+    {
+        $this->users = new User();
+    }
+
+    public function index(){
+        // return view('login');
+        return view('master');
+    }
+
+}
