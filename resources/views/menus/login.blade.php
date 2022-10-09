809e318084730c782f6c7d81664a90a466df7f47
Rizki Amanullah Hakim <rizkiamanullah723@gmail.com>
2022-10-09 20:07:28 +0700

    recaptcha moved to top login

--- resources/views/menus/login.blade.php
+++ resources/views/menus/login.blade.php
@@ -18,6 +18,12 @@
                     <button type="button" class="close" data-dismiss="alert">×</button>	
                 </div>
                 @endif
+                @if($errors->has('g-recaptcha-response'))
+                <div class="px-3 py-2 w-full flex justify-between rounded border-red-800 bg-red-500 font-thin">
+                    <small class="text-danger">{{ $errors->first('g-recaptcha-response') }}</small>
+                    <button type="button" class="close" data-dismiss="alert">×</button>	
+                </div>
+                @endif
                 <div>
                     <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
                     <input type="email" value="{{old('email')}}" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required="">
@@ -36,12 +42,6 @@
                     </div>
                     <a href="#" class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
                 </div> --}}
-                @if($errors->has('g-recaptcha-response'))
-                <div class="px-3 py-2 w-full flex justify-between rounded border-red-800 bg-red-500 font-thin">
-                    <small class="text-danger">{{ $errors->first('g-recaptcha-response') }}</small>
-                    <button type="button" class="close" data-dismiss="alert">×</button>	
-                </div>
-                @endif
                 <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
                 <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
                     Not registered? <a href="create_account" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
