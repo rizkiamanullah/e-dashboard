55695480be2174c4341d0c63d1ecfe2fc870686d
Rizki Amanullah Hakim <rizkiamanullah723@gmail.com>
2022-10-09 20:50:22 +0700

    added skeleton

---
+++ resources/views/menus/inbox.blade.php
@@ -0,0 +1,12 @@
+@extends('master.master')
+@section('content')
+<div class="content">
+    <div class="p-6 w-full border rounded border-gray-700 bg-gray-800 text-white">
+        {{-- Edit here --}}
+        
+        {{-- Edit end here --}}
+    </div>
+</div>
+@endsection
+@section('js')
+@endsection
--- resources/views/menus/main.blade.php
+++ resources/views/menus/main.blade.php
@@ -1,15 +1,33 @@
 @extends('master.master')
 @section('content')
 <div class="content">
+    {{-- <div role="status" class="space-y-8 animate-pulse md:space-y-0 md:space-x-8 md:flex md:items-center">
+        <div class="w-full">
+            <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[480px] mb-2.5"></div>
+            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[440px] mb-2.5"></div>
+            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[460px] mb-2.5"></div>
+            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+        </div>
+        <span class="sr-only">Loading...</span>
+    </div> --}}
     <div class="p-6 w-full border rounded border-gray-700 bg-gray-800 text-white">
-        <div class=" space-y-3">
-            <h1 class=" font-bold">Selamat Datang di E-Dashboard</h1>
-            <div class="rounded-md bg-gradient-to-r from-blue-300 via-purple-300 to-orange-400">
-            @for ($i = 0; $i <= 10; $i++)
-                <br>
-            @endfor
+        <div class="space-y-3 main-skeleton">
+            <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+            <div class="flex animate-pulse justify-center items-center w-full h-48 bg-gray-300 rounded sm:w-96 dark:bg-gray-700">
+                <svg class="w-full h-12 text-gray-200" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" fill="currentColor" viewBox="0 0 640 512"><path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z"/></svg>
             </div>
-            <div class=" font-light">
+            <div class="p-2">
+                <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+            </div>
+        </div>
+        <div class="space-y-3 main-content">
+            <h1 class=" font-bold">Selamat Datang di E-Dashboard</h1>
+            <div class="rounded-md h-48 w-full bg-gradient-to-r from-blue-300 via-purple-300 to-orange-400">
+                <p class=" text-gray-800 font-bold text-9xl">E-Dashboard</p>
+            </div>
+            <div class="font-light p-2">
                 Silahkan gunakan sidebar untuk melihat hal yang ditawarkan oleh dashboard ini.
             </div>
         </div>
